2f08e141b91277cfa07e3b476c700ff173ddb6c3
Alvaro Isorna <alvaro.isorna@gmail.com>
2022-11-30 11:53:11 +0100

    chore: :tada: base project configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jest'
+  ],
+  rules: {
+  }
+}
