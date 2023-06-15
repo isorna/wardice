7c13421362d70ccb53875e44e64a31b5d1a3572a
Alvaro Isorna <alvaro.isorna@gmail.com>
2023-06-15 15:38:57 +0200

    feat(40k pages): :sparkles: jun 15 indexes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ import AdeptusAstartesPage from '@/views/40k/AdeptusAstartesPage.vue'
 import AdeptusCustodesPage from '@/views/40k/AdeptusCustodesPage.vue'
 import AdeptusMechanicusPage from '@/views/40k/AdeptusMechanicusPage.vue'
 import AdeptaSororitasPage from '@/views/40k/AdeptaSororitasPage.vue'
+import AeldariPage from '@/views/40k/AeldariPage.vue'
 import AgentsImperiumPage from '@/views/40k/AgentsImperiumPage.vue'
 import AstraMilitarumPage from '@/views/40k/AstraMilitarumPage.vue'
 import BlackTemplarsPage from '@/views/40k/BlackTemplarsPage.vue'
@@ -14,9 +15,15 @@ import ChaosSpaceMarinesPage from '@/views/40k/ChaosSpaceMarinesPage.vue'
 import DarkAngelsPage from '@/views/40k/DarkAngelsPage.vue'
 import DeathGuardPage from '@/views/40k/DeathGuardPage.vue'
 import DeathwatchPage from '@/views/40k/DeathwatchPage.vue'
+import DrukhariPage from '@/views/40k/DrukhariPage.vue'
+import GenestealerCultsPage from '@/views/40k/GenestealerCultsPage.vue'
 import GreyKnightsPage from '@/views/40k/GreyKnightsPage.vue'
 import ImperialKnightsPage from '@/views/40k/ImperialKnightsPage.vue'
+import LeaguesVotannPage from '@/views/40k/LeaguesVotannPage.vue'
+import NecronsPage from '@/views/40k/NecronsPage.vue'
+import OrksPage from '@/views/40k/OrksPage.vue'
 import SpaceWolvesPage from '@/views/40k/SpaceWolvesPage.vue'
+import TauEmpirePage from '@/views/40k/TauEmpirePage.vue'
 import ThousandSonsPage from '@/views/40k/ThousandSonsPage.vue'
 import TyranidsPage from '@/views/40k/TyranidsPage.vue'
 import WorldEatersPage from '@/views/40k/WorldEatersPage.vue'
@@ -34,6 +41,11 @@ export const routes = [
     component: HomePage40k
   },
   {
+    path: '/40k/adepta-sororitas',
+    name: 'Adepta Sororitas',
+    component: AdeptaSororitasPage
+  },
+  {
     path: '/40k/adeptus-astartes',
     name: 'Adeptus Astartes',
     component: AdeptusAstartesPage
@@ -49,9 +61,9 @@ export const routes = [
     component: AdeptusMechanicusPage
   },
   {
-    path: '/40k/adepta-sororitas',
-    name: 'Adepta Sororitas',
-    component: AdeptaSororitasPage
+    path: '/40k/aledari',
+    name: 'Aeldari',
+    component: AeldariPage
   },
   {
     path: '/40k/agents-imperium',
@@ -104,14 +116,39 @@ export const routes = [
     component: DeathwatchPage
   },
   {
+    path: '/40k/drukhari',
+    name: 'Drukhari',
+    component: DrukhariPage
+  },
+  {
+    path: '/40k/genestealer-cults',
+    name: 'Genestealer Cults',
+    component: GenestealerCultsPage
+  },
+  {
     path: '/40k/grey-knights',
-    name: 'GreyKnights',
+    name: 'Grey Knights',
     component: GreyKnightsPage
   },
   {
     path: '/40k/imperial-knights',
-    name: 'ImperialKnights',
+    name: 'Imperial Knights',
     component: ImperialKnightsPage
+  },
+  {
+    path: '/40k/leagues-votann',
+    name: 'Leagues of Votann',
+    component: LeaguesVotannPage
+  },
+  {
+    path: '/40k/necrons',
+    name: 'Necrons',
+    component: NecronsPage
+  },
+  {
+    path: '/40k/orks',
+    name: 'Orks',
+    component: OrksPage
   },
   {
     path: '/40k/space-wolves',
@@ -120,8 +157,13 @@ export const routes = [
   },
   {
     path: '/40k/thousand-sons',
-    name: 'ThousandSonsPage',
+    name: 'Thousand Sons',
     component: ThousandSonsPage
+  },
+  {
+    path: '/40k/tau-empire',
+    name: 'T\'au Empire',
+    component: TauEmpirePage
   },
   {
     path: '/40k/tyranids',
--- src/views/40k/HomePage.vue
+++ src/views/40k/HomePage.vue
@@ -4,10 +4,11 @@
     <section class="page">
       <h1>Select a faction</h1>
       <ul>
+        <li><router-link to="/40k/adepta-sororitas">Adepta Sororitas</router-link></li>
         <li><router-link to="/40k/adeptus-astartes">Adeptus Astartes</router-link></li>
         <li><router-link to="/40k/adeptus-custodes">Adeptus Custodes</router-link></li>
         <li><router-link to="/40k/adeptus-mechanicus">Adeptus Mechanicus</router-link></li>
-        <li><router-link to="/40k/adepta-sororitas">Adepta Sororitas</router-link></li>
+        <li><router-link to="/40k/aeldari">Aeldari</router-link></li>
         <li><router-link to="/40k/agents-imperium">Agents of the Imperium</router-link></li>
         <li><router-link to="/40k/astra-militarum">Astra Militarum</router-link></li>
         <li><router-link to="/40k/black-templars">Black Templars</router-link></li>
@@ -18,9 +19,15 @@
         <li><router-link to="/40k/dark-angels">Dark Angels</router-link></li>
         <li><router-link to="/40k/death-guard">Death Guard</router-link></li>
         <li><router-link to="/40k/deathwatch">Deathwatch</router-link></li>
+        <li><router-link to="/40k/drukhari">Drukhari</router-link></li>
+        <li><router-link to="/40k/genestealer-cults">Genestealer Cults</router-link></li>
         <li><router-link to="/40k/grey-knights">Grey Knights</router-link></li>
         <li><router-link to="/40k/imperial-knights">Imperial Knights</router-link></li>
+        <li><router-link to="/40k/leagues-votann">Leagues of Votann</router-link></li>
+        <li><router-link to="/40k/necrons">Necrons</router-link></li>
+        <li><router-link to="/40k/orks">Orks</router-link></li>
         <li><router-link to="/40k/space-wolves">Space Wolves</router-link></li>
+        <li><router-link to="/40k/tau-empire">T'au Empire</router-link></li>
         <li><router-link to="/40k/thousand-sons">Thousand Sons</router-link></li>
         <li><router-link to="/40k/tyranids">Tyranids</router-link></li>
         <li><router-link to="/40k/world-eaters">World Eaters</router-link></li>
